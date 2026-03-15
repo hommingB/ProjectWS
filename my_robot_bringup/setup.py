8e829f9a3b783e3e884077a7df8e88e3b86b18fd
hommingB <hobblingheli@gmail.com>
2026-03-15 04:10:47 +0000

    add nav2 thing

--- my_robot_bringup/setup.py
+++ my_robot_bringup/setup.py
@@ -12,7 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.pgm'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
